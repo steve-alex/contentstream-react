b5aa83a261078b340e949442edf7c27d3e4e1628
steve-alex <steve.alex@hotmail.co.uk>
2019-12-03 10:57:10 +0000

    Created getting tweets

--- src/containers/postsContainer.js
+++ src/containers/postsContainer.js
@@ -1,17 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { Dropdown, Menu, Button, Input } from 'semantic-ui-react'
 import { isUpdateExpression } from '@babel/types';
+const BASE_URL = "http://localhost:3001"
 
 const PostsContainer = () => {
-    const [buckets, setBuckets] = useState("")
-    const [selected, setSelectied] = useState("")
+// x    const [selected, setSelectied] = useState("")
 
     useEffect(() => {
       //Get the buckets for a specific user//
     }, [])
 
+    const getTweets = (e) => {
+      e.preventDefault()
+      return fetch(`${BASE_URL}/twitter/timeline`, {
+        headers: {
+          Authorisation: localStorage.getItem("token"),
+        }
+      })
+        .then(resp => resp.json())
+        .then(console.log)
+    }
+
     return (
       <>
+        <button onClick={getTweets}>Get tweets</button>
       </>
     )
 
