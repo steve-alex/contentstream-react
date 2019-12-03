20333ad42f9c269eb5ff1d125d61076a6701484d
steve-alex <steve.alex@hotmail.co.uk>
2019-12-03 15:08:09 +0000

    created new buckets function

--- src/containers/menuContainer.js
+++ src/containers/menuContainer.js
@@ -1,14 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import { Dropdown, Menu, Button, Input, Grid } from 'semantic-ui-react'
 import { isUpdateExpression } from '@babel/types';
+import jsonify from '../adapters/API.js'
 
 const MenuContainer = (props) => {
     const [buckets, setBuckets] = useState("")
+    const [bucketName, setBucketName] = useState("")
     // const [selected, setSelectied] = useState("")
 
     useEffect(() => {
       //Get the buckets for a specific user//
     }, [])
+
+    const createNewBucket = (e) => {
+      e.preventDefault()
+      console.log(bucketName)
+      return fetch("http://localhost:3001/buckets", {
+        type: "POST",
+        headers: {
+          "Content-Type": "Application/json",
+          "Accept": "Application/json",
+          "authorisation": localStorage.token
+        },
+        body: {
+          name: bucketName
+        }
+      })
+      .then(jsonify)
+      .then(console.log)
+      // .then(newBucket => setBuckets([...buckets, newBucket]))
+    }
 
     return (
       <>
@@ -32,7 +53,21 @@ const MenuContainer = (props) => {
             active={props.selected === "Account"}
             onClick={(e) => props.setSelected("Account")}
           />
+          <Menu.Item
+            name='Account'
+            active={props.selected === "Create Bucket"}
+            onClick={(e) => props.setSelected("Account")}
+          />
           </Menu>
+
+          <form onSubmit={createNewBucket}>
+            <input
+                type="text"
+                name="name"
+                value={bucketName}
+                onChange={(e) => setBucketName(e.target.value)}/>
+            <input type="submit" value="Submit" />
+          </form>
       </>
     )
 
