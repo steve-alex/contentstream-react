b3122104d5856539e81ded505b5b601d6db13546
idmyn <hello@davidmyno.rs>
2019-12-04 09:30:08 +0000

    properly handle backend response, redirecting to twitter auth url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Route } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
 import AuthPage from './pages/authPage.js'
 import HomePage from './pages/homePage.js'
 import API from './adapters/API.js'
@@ -10,6 +10,25 @@ const App = ({history}) => {
   const [user, setUser] = useState(null)
 
   useEffect(() => {
+    const twitterString = window.location.search.split('?')[1]
+    if (twitterString) {
+      const twitterParams = twitterString.split('&').reduce((memo, param) => {
+        const split = param.split('=')
+        memo[split[0]] = split[1]
+        return memo
+      }, {})
+      console.log(twitterParams)
+      fetch('http://localhost:3001/twitter/success', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          'Authorisation': localStorage.token
+        },
+        body: JSON.stringify(twitterParams)
+      })
+    }
+
     API.validate(user)
       .then(user => {
         setUser(user)
