e0bb5655a272cba4b5cc9ba2ce749fd31bfe0510
idmyn <hello@davidmyno.rs>
2019-12-04 19:22:38 +0000

    tidy menuContainer and add key to bucket map

--- src/containers/menuContainer.js
+++ src/containers/menuContainer.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Dropdown, Menu, Button, Input, Grid, Sticky } from 'semantic-ui-react'
-import { isUpdateExpression } from '@babel/types';
+import { isUpdateExpression } from '@babel/types'
 import jsonify from '../adapters/API.js'
 import BucketsContainer from './bucketsContainer.js'
 import API from '../adapters/API.js'
@@ -8,124 +8,130 @@ import API from '../adapters/API.js'
 const twitterLogin = () => {
   fetch('http://localhost:3001/twitter/login', {
     headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-      "Authorisation": localStorage.token
-    }
-  }).then(res => res.json()).then(res => {
-    if (typeof res === 'string' && res.startsWith('https://api.twitter.com/oauth/authorize?oauth_token=')) {
-      window.location.href = res
-    } else {
-      console.log(res)
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorisation: localStorage.token
     }
   })
+    .then(res => res.json())
+    .then(res => {
+      if (
+        typeof res === 'string' &&
+        res.startsWith('https://api.twitter.com/oauth/authorize?oauth_token=')
+      ) {
+        window.location.href = res
+      } else {
+        console.log(res)
+      }
+    })
 }
 
-const MenuContainer = (props) => {
-    const [buckets, setBuckets] = useState("")
-    const [bucketName, setBucketName] = useState("")
+const MenuContainer = props => {
+  const [buckets, setBuckets] = useState('')
+  const [bucketName, setBucketName] = useState('')
 
-    const createNewBucket = (e) => {
-      e.preventDefault()
-      return fetch("http://localhost:3001/buckets", {
-        method: "POST",
-        headers: {
-          "Content-Type": "Application/json",
-          "Accept": "Application/json",
-          "authorisation": localStorage.token
-        },
-        body: JSON.stringify({
-          name: bucketName
-        })
+  const createNewBucket = e => {
+    e.preventDefault()
+    return fetch('http://localhost:3001/buckets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/json',
+        Accept: 'Application/json',
+        authorisation: localStorage.token
+      },
+      body: JSON.stringify({
+        name: bucketName
       })
-      .then(resp => API.jsonify(resp))
+    })
+      .then(resp => resp.json())
       .then(console.log)
-      // .then(newBucket => setBuckets([...buckets, newBucket]))
-    }
+    // .then(newBucket => setBuckets([...buckets, newBucket]))
+  }
 
-    const onDragOver = (e) => {
-      e.preventDefault()
-      e.target.style.color = "red"
-    }
+  const onDragOver = e => {
+    e.preventDefault()
+    e.target.style.color = 'red'
+  }
 
-    const onDragLeave = (e) => {
-      e.preventDefault()
-      e.target.style.color = 'grey'
-    }
+  const onDragLeave = e => {
+    e.preventDefault()
+    e.target.style.color = 'grey'
+  }
 
-    const onDrop = (e, bucketId) => {
-      e.preventDefault()
-      e.target.style.color = 'grey'
-      console.log(bucketId)
-      console.log(props.selectedTweet)
-      return fetch("http://localhost:3001/posts", {
-        method: "POST",
-        headers: {
-          "Content-Type": "Application/json",
-          "Accept": "Application/json",
-          "authorisation": localStorage.token
-        },
-        body: JSON.stringify({
-          bucketId: bucketId,
-          sourceId: props.selectedTweet,
-          domain: "twitter.com"
-        })
+  const onDrop = (e, bucketId) => {
+    e.preventDefault()
+    e.target.style.color = 'grey'
+    console.log(bucketId)
+    console.log(props.selectedTweet)
+    return fetch('http://localhost:3001/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/json',
+        Accept: 'Application/json',
+        Authorisation: localStorage.token
+      },
+      body: JSON.stringify({
+        bucketId: bucketId,
+        sourceId: props.selectedTweet,
+        domain: 'twitter.com'
       })
+    })
       .then(API.jsonify)
       .then(console.log)
-    }
+  }
 
-    return (
-      <>
-        <Menu pointing vertical >
-          <Menu.Item
-            name='Home'
-            active={props.selected === "Home"}
-            onClick={(e) => props.setSelected("Home")}
-          />
-          <Menu.Item
-            name='Feed Search'
-            active={props.selected ==="Feed"}
-            onClick={(e) => props.setSelected("Feed")}
-          />
-          <Menu.Item
-            draggable
-            name='Account'
-            active={props.selected === "Account"}
-            onClick={(e) => props.setSelected("Account")}
-          />
-          <Menu.Item
-            name='Buckets'
-          />
-          <Menu.Menu>
+  return (
+    <>
+      <Menu pointing vertical>
+        <Menu.Item
+          name="Home"
+          active={props.selected === 'Home'}
+          onClick={e => props.setSelected('Home')}
+        />
+        <Menu.Item
+          name="Feed Search"
+          active={props.selected === 'Feed'}
+          onClick={e => props.setSelected('Feed')}
+        />
+        <Menu.Item
+          draggable
+          name="Account"
+          active={props.selected === 'Account'}
+          onClick={e => props.setSelected('Account')}
+        />
+        <Menu.Item name="Buckets" />
+        <Menu.Menu>
           {/* <BucketsContainer
             selected={props.selected}
             setSelected={props.selected}/> */}
           {props.buckets.map(bucket => {
-            return <Menu.Item
-                      onDrop={(e) => onDrop(e, bucket.id)}
-                      onDragOver={(e) => onDragOver(e)}
-                      onDragLeave={(e) => onDragLeave(e)}
-                      name={bucket.name}
-                      active={props.selected === bucket.id}
-                      onClick={(e) => props.setSelected(bucket.id)}
-                    />})
-          }
-          </Menu.Menu>
-        </Menu>
-          <form onSubmit={createNewBucket}>
-            <input
-                type="text"
-                name="name"
-                value={bucketName}
-                onChange={(e) => setBucketName(e.target.value)}/>
-            <input type="submit" value="Submit" />
+            return (
+              <Menu.Item
+                key={bucket.id}
+                onDrop={e => onDrop(e, bucket.id)}
+                onDragOver={e => onDragOver(e)}
+                onDragLeave={e => onDragLeave(e)}
+                name={bucket.name}
+                active={props.selected === bucket.id}
+                onClick={e => props.setSelected(bucket.id)}
+              />
+            )
+          })}
+        </Menu.Menu>
+      </Menu>
+      <form onSubmit={createNewBucket}>
+        <input
+          type="text"
+          name="name"
+          value={bucketName}
+          onChange={e => setBucketName(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
 
-          </form>
-
-        <button onClick={twitterLogin}>log in to twitter</button>
-      </>
-    )
+      <button onClick={twitterLogin}>log in to twitter</button>
+    </>
+  )
 }
 
-export default MenuContainer;
+export default MenuContainer
