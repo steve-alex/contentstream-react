868489369812685b8abf2bca2c3ec0404b5b3bd0
steve-alex <steve.alex@hotmail.co.uk>
2019-12-04 18:53:10 +0000

    minor changes

--- src/containers/bucketsContainer.js
+++ src/containers/bucketsContainer.js
@@ -10,7 +10,7 @@ const BucketsContainer = (props) => {
   }, [])
 
   const getBucketTweets = () => {
-    
+
   }
 
   return (
@@ -19,7 +19,7 @@ const BucketsContainer = (props) => {
         tweetIds.map(tweetId => {
           return <Tweet
             tweetId={tweetId}
-            key={tweet.id}
+            key={tweetId}
           />
         })
       }
