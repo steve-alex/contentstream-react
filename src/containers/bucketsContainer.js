ca36eb603bd13496dd2bb51164b266135138a8e1
idmyn <hello@davidmyno.rs>
2019-12-04 19:22:17 +0000

    get buckets rendering their tweets

--- src/containers/bucketsContainer.js
+++ src/containers/bucketsContainer.js
@@ -1,30 +1,46 @@
 import React, { useState, useEffect } from 'react'
-import { Dropdown, Menu, Button, Input, Grid, Sticky, Item} from 'semantic-ui-react'
+import {
+  Dropdown,
+  Menu,
+  Button,
+  Input,
+  Grid,
+  Sticky,
+  Item
+} from 'semantic-ui-react'
 import Tweet from '../components/tweet.js'
 
-const BucketsContainer = (props) => {
-  const [tweetIds, setTweetsIds] = useState([])
-  
-  useEffect(() => {
-    getBucketTweets()
-  }, [])
+const getBucketTweets = (bucketId, setTweetIds) => {
+  fetch('http://localhost:3001/buckets/' + bucketId, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorisation: localStorage.token
+    }
+  })
+    .then(res => res.json())
+    .then(bucket => {
+      setTweetIds(bucket.posts.map(post => post.sourceId))
+    })
+}
 
-  const getBucketTweets = () => {
-    
-  }
+const BucketsContainer = props => {
+  const [tweetIds, setTweetIds] = useState([])
+
+  useEffect(
+    () => {
+      getBucketTweets(props.selected, setTweetIds)
+    },
+    [props.selected]
+  )
 
   return (
     <>
-      {
-        tweetIds.map(tweetId => {
-          return <Tweet
-            tweetId={tweetId}
-            key={tweet.id}
-          />
-        })
-      }
+      {tweetIds.map(tweetId => {
+        return <Tweet tweetId={tweetId} key={tweetId} />
+      })}
     </>
   )
 }
 
-export default BucketsContainer;
+export default BucketsContainer
